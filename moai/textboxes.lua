91da63d509b51ecc99bda90e5092a03a536eea18
dennislu <dennis3dlu@gmail.com>
2013-07-06 13:18:09 -0700

    adding text

--- moai/textboxes.lua
+++ moai/textboxes.lua
@@ -1,3 +1,7 @@
+
+Textboxes = class()
+
+function Textboxes:init(world, x, y)
 MOAISim.openWindow ( "Textboxes", 320, 480 )
  
 viewport = MOAIViewport.new ()
@@ -6,3 +10,14 @@ viewport:setSize ( 320, 480 )
 layer = MOAILayer2D.new ()
 layer:setViewport ( viewport )
 MOAISim.pushRenderPass ( layer )
+
+font = MOAIFont.new ()
+font:loadFromTTF ( 'Bastarda-K.ttf', charcodes, 16, 163 )
+
+textbox = MOAITextBox.new ()
+textbox:setString ( "This is our text for the tutorial over textboxes" )
+textbox:setFont ( font )
+textbox:setTextSize ( font:getScale ())
+textbox:setRect ( -150, -230, 150, 230 )
+textbox:setYFlip ( true )
+layer:insertProp ( textbox )
