663835e1063a8d9bd4063e71b460945230000387
Tom Thompson <tom@codethulu.com>
2013-07-06 03:59:49 -0700

    basic platforming.. jumps are a bit wonky

---
+++ moai/dude.lua
@@ -0,0 +1,79 @@
+require 'class'
+
+lcontact = nil
+rcontact = nil
+
+Dude = class()
+
+function Dude:init(world, x, y)
+	self.body = world:addBody(MOAIBox2DBody.DYNAMIC)
+	self.rect = self.body:addRect(-10, -10, 10, 10)
+	self.body:setFixedRotation()
+	self.body:setTransform(x, y)
+	self.lsensor = self.body:addRect(-9, 8, -8, 12)
+	self.rsensor = self.body:addRect(8, 8, 9, 12)
+	self.lsensor:setSensor()
+	self.rsensor:setSensor()
+
+	self.move_dir = {}
+
+	self.lsensor:setCollisionHandler(handleLCollision, MOAIBox2DArbiter.BEGIN + MOAIBox2DArbiter.END)
+	self.rsensor:setCollisionHandler(handleRCollision, MOAIBox2DArbiter.BEGIN + MOAIBox2DArbiter.END)
+end
+
+function Dude:move(dir)
+	if dir == 'left' then
+		self.move_dir['left'] = true
+	else
+		self.move_dir['right'] = true
+	end
+	self:setDir()
+end
+
+function Dude:unMove(dir)
+	if dir == 'left' then
+		self.move_dir['left'] = nil
+	else
+		self.move_dir['right'] = nil
+	end
+	self:setDir()
+end
+
+function Dude:setDir()
+	velX, velY = self.body:getLinearVelocity()
+	if self.move_dir.left then
+		self.body:setLinearVelocity(-50, velY)
+		self.rect:setFriction(0)
+	elseif self.move_dir.right then
+		self.body:setLinearVelocity(50, velY)
+		self.rect:setFriction(0)
+	else
+		self.body:setLinearVelocity(0, velY)
+		self.rect:setFriction(0.5)
+	end
+end
+
+function Dude:jump()
+	if lcontact or rcontact then
+		velX, velY = self.body:getLinearVelocity()
+--		self.body:setLinearVelocity(velX, -100)
+		self.body:applyLinearImpulse(0, -50)
+	end
+end
+
+function handleLCollision(phase, a, b, arbiter)
+	if phase == MOAIBox2DArbiter.BEGIN then
+		lcontact = true
+	elseif phase == MOAIBox2DArbiter.END then
+		lcontact = nil
+	end
+end
+
+function handleRCollision(phase, a, b, arbiter)
+	if phase == MOAIBox2DArbiter.BEGIN then
+		rcontact = true
+	elseif phase == MOAIBox2DArbiter.END then
+		rcontact = nil
+	end
+end
+
---
+++ moai/main.lua
@@ -0,0 +1,46 @@
+require 'dude'
+
+MOAISim.openWindow("foo", 1280, 720)
+viewport = MOAIViewport.new()
+viewport:setSize(1280, 720)
+viewport:setScale(1280, -720)
+
+layer = MOAILayer2D.new()
+layer:setViewport(viewport)
+MOAISim.pushRenderPass(layer)
+
+world = MOAIBox2DWorld.new()
+world:setGravity(0, 10)
+world:setUnitsToMeters(2)
+world:start()
+layer:setBox2DWorld(world)
+
+worldBody = world:addBody(MOAIBox2DBody.STATIC)
+fixture = worldBody:addRect(-500, 15, 500, 20)
+fixture:setFilter(0x02)
+
+stickman = Dude(world, -100, -200)
+
+
+function keyboardEvent(key, down)
+	if key == 119 then -- w
+		stickman:jump()
+
+	elseif key == 114 then -- r
+
+	elseif key == 97 then -- a
+		if down then
+			stickman:move('left')
+		else
+			stickman:unMove('left')
+		end
+	elseif key == 115 then -- s
+		if down then
+			stickman:move('right')
+		else
+			stickman:unMove('right')
+		end
+	end
+end
+
+MOAIInputMgr.device.keyboard:setCallback(keyboardEvent)
